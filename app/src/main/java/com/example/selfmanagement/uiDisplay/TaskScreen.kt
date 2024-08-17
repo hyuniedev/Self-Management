0980e1d489f00d891804d8c843a6b794a6afdaa8
NguyenHuuNien <huunien734@gmail.com>
2024-08-17 22:33:36 +0700

    + Add Top App Bar

--- app/src/main/java/com/example/selfmanagement/uiDisplay/TaskScreen.kt
+++ app/src/main/java/com/example/selfmanagement/uiDisplay/TaskScreen.kt
@@ -1,9 +1,80 @@
 package com.example.selfmanagement.uiDisplay
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Checklist
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material.icons.rounded.SelectAll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 
-@Composable
-fun TaskScreen(navController: NavHostController){
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TaskScreen(navController: NavHostController) {
+    var isSearch = remember {
+        mutableStateOf(false)
+    }
+    var isSelect = remember {
+        mutableStateOf(false)
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "My Tasks") },
+                actions = {
+                    IconButton(onClick = {
+                        isSearch.value = !isSearch.value
+                    }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.Search,
+                            contentDescription = "Search"
+                        )
+                    }
+                    IconButton(
+                        onClick = {
+                            isSelect.value = !isSelect.value
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.Checklist,
+                            contentDescription = "Select"
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.inverseOnSurface
+                )
+            )
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            // Tim kiem
+
+            // Danh sach task
+        }
+    }
 }
