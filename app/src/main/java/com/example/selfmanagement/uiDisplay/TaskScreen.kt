c493755df03521df3f9214e3bf40dc87e8dd9009
NguyenHuuNien <huunien734@gmail.com>
2024-08-18 21:12:19 +0700

    ~ Customize Card Task

--- app/src/main/java/com/example/selfmanagement/uiDisplay/TaskScreen.kt
+++ app/src/main/java/com/example/selfmanagement/uiDisplay/TaskScreen.kt
@@ -3,14 +3,18 @@ package com.example.selfmanagement.uiDisplay
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,6 +23,7 @@ import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.ChevronRight
 import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,6 +31,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -37,6 +43,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,6 +52,10 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import com.example.selfmanagement.data.Task
 import com.example.selfmanagement.data.tagGroup
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 
 val lsGroupTask = listOf(
@@ -155,10 +167,8 @@ fun GroupToDo(indexTitle: Int, indexDisplay: MutableIntState) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 10.dp)
-                    .padding(bottom = 10.dp)
-                    .clip(RoundedCornerShape(6.dp))
-                    .background(color = MaterialTheme.colorScheme.inverseOnSurface),
+                    .padding(10.dp)
+                    .clip(RoundedCornerShape(6.dp)),
             ) {
                 // TODO("them items cac task va goi CardTask()")
                 val tmpTask: Task = Task(
@@ -175,33 +185,74 @@ fun GroupToDo(indexTitle: Int, indexDisplay: MutableIntState) {
 
 @Composable
 fun CardTask(task: Task) {
-    Column {
+    Column(
+        modifier = Modifier
+            .clip(RoundedCornerShape(6.dp))
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.inverseOnSurface)
+            .padding(horizontal = 10.dp, vertical = 6.dp)
+    ) {
         LazyRow {
             task.lsTag?.forEach {
                 // TODO("Add Composable Tags")
             }
         }
-        Text(text = task.name)
-        Text(text = task.description)
-        Row {
-            Text(text = "Start")
-            Text(text = task.timeStart.time.toString())
-        }
-        Row {
-            Text(text = "End")
-            Text(text = task.timeEnd.time.toString())
-        }
-        Row {
-            Text(text = "Reminder")
-            Text(text = task.reminder.time.toString())
-        }
-        Row {
-            Button(onClick = {}) {
-                Text(text = "Edit")
-            }
-            Button(onClick = {}) {
+        Spacer(modifier = Modifier.height(6.dp))
+        Text(
+            text = task.name,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 24.sp,
+            color = Color.DarkGray
+        )
+        Spacer(modifier = Modifier.height(6.dp))
+        Text(
+            text = task.description,
+            fontSize = 18.sp,
+            fontStyle = FontStyle.Italic,
+            color = Color.Gray
+        )
+        Spacer(modifier = Modifier.height(14.dp))
+        RowTime(title = "Start:", time = getCurrentDateTime(task.timeStart.time))
+        RowTime(title = "End:", time = getCurrentDateTime(task.timeEnd.time))
+        RowTime(title = "Reminder:", time = getCurrentDateTime(task.reminder.time))
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            OutlinedButton(onClick = {}) {
                 Text(text = "Move")
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+            OutlinedButton(
+                onClick = {}
+            ) {
+                Text(text = "Edit")
             }
         }
     }
+}
+
+@Composable
+private fun RowTime(title: String, time: String){
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Text(
+            text = title,
+            fontSize = 18.sp
+        )
+        Text(
+            text = time,
+            fontSize = 18.sp,
+            fontStyle = FontStyle.Italic
+        )
+    }
+}
+
+fun getCurrentDateTime(time: Date): String {
+    val formatter = SimpleDateFormat("dd/MM/yyyy, HH:mm", Locale.getDefault())
+    return formatter.format(time)
 }
